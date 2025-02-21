0f0d170b24b765259081dead6da8ff0bf3094e15
Alan Płócieniak <alan-null@outlook.com>
2025-02-21 23:42:33 +0100

    ✅ Approved

--- 3_0_5_0/js/whats-new.js
+++ 3_0_5_0/js/whats-new.js
@@ -14,5 +14,20 @@
 ##  © MRFDEV.com - All Rights Reserved
 ##
 */
-(a=>{chrome.storage.local.get({localecode:chrome.i18n.getMessage("locale_code")},c=>{var b=0>"bg ca cs da de el es et fi fr hr hu it lt lv nl pl pt_PT ro sk sl sv".indexOf(c.localecode)?"USD":"EUR";a.querySelector(".thanks-buttons").classList.add(b);a.querySelectorAll(".thanks-buttons button").forEach(d=>{d.addEventListener("click",function(){chrome.tabs.create({url:`https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=JYKCHGNJ6PFC4&item_name=Enhancer%20for%20YouTube%E2%84%A2%20%5BChrome%20Extension%5D&no_note=1&no_shipping=1&rm=1&return=https%3A%2F%2Fwww.mrfdev.com%2Fthanks&cancel_return=https%3A%2F%2Fwww.mrfdev.com%2Fdonate&amount=${this.dataset.amount}&currency_code=${b}`,
-active:!0})})})})})(document);
+(document => {
+    chrome.storage.local.get({
+        localecode: chrome.i18n.getMessage("locale_code")
+    }, ({ localecode }) => {
+        const currency = ["bg", "ca", "cs", "da", "de", "el", "es", "et", "fi", "fr", "hr", "hu", "it", "lt", "lv", "nl", "pl", "pt_PT", "ro", "sk", "sl", "sv"].includes(localecode) ? "EUR" : "USD";
+        const thanksButtons = document.querySelector(".thanks-buttons");
+        thanksButtons.classList.add(currency);
+        thanksButtons.querySelectorAll("button").forEach(button => {
+            button.addEventListener("click", () => {
+                chrome.tabs.create({
+                    url: `https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=JYKCHGNJ6PFC4&item_name=Enhancer%20for%20YouTube%E2%84%A2%20%5BChrome%20Extension%5D&no_note=1&no_shipping=1&rm=1&return=https%3A%2F%2Fwww.mrfdev.com%2Fthanks&cancel_return=https%3A%2F%2Fwww.mrfdev.com%2Fdonate&amount=${button.dataset.amount}&currency_code=${currency}`,
+                    active: true
+                });
+            });
+        });
+    });
+})(document);
