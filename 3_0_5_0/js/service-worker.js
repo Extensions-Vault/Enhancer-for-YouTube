1468e3500f0b01ab2c526ac0a7066d7b4fe068df
Alan Płócieniak <alan-null@outlook.com>
2025-02-22 10:05:59 +0100

    ✅ Approved

--- 3_0_5_0/js/service-worker.js
+++ 3_0_5_0/js/service-worker.js
@@ -15,12 +15,175 @@
 ##
 */
 importScripts("config.js");
-(f=>{chrome.action.onClicked.addListener(()=>{chrome.runtime.openOptionsPage()});chrome.runtime.setUninstallURL("https://www.mrfdev.com/event?n=uninstall&b=chrome&e=enhancer-for-youtube&v="+chrome.runtime.getManifest().version);chrome.runtime.onInstalled.addListener(async c=>{if("install"===c.reason)chrome.runtime.openOptionsPage(),chrome.tabs.create({url:"https://www.mrfdev.com/event?n=install&b=chrome&e=enhancer-for-youtube&v="+chrome.runtime.getManifest().version,active:!0}),chrome.storage.local.set({date:Date.now(),
-update:Date.now()});else if("update"===c.reason){var a=await chrome.storage.local.get(),b={};/2\.0\.(10[3-9]|11[0-9]|12[0-1])/.test(c.previousVersion)&&(Array.isArray(a.controls)&&(b.controls=a.controls.filter(d=>"whitelist"!==d&&"not-interested"!==d)),await chrome.storage.local.remove(["blockads","blockadsexceptforsubs","whitelist"]));if(/^2\.0/.test(c.previousVersion)){b.reload=!0;b.whatsnew=!0;b.videofilters=f.videofilters;var e="blur brightness contrast grayscale huerotate invert saturate sepia".split(" ");
-e.forEach(d=>{a[d]&&("huerotate"===d?b.videofilters.rotation=a[d]:"invert"===d?b.videofilters.inversion=a[d]:"saturate"===d?b.videofilters.saturation=a[d]:b.videofilters[d]=a[d])});a.backgroundcolor&&(b.backdropcolor=a.backgroundcolor,e.push("backgroundcolor"));a.backgroundopacity&&(b.backdropopacity=a.backgroundopacity,e.push("backgroundopacity"));a.customcssrules&&(b.customcss=a.customcssrules,e.push("customcssrules"));await chrome.storage.local.remove(e);a.miniplayersize&&(b.miniplayersize=a.miniplayersize.replace("_",
-""));a.miniplayerposition&&(b.miniplayerposition=a.miniplayerposition.replace("_",""));a.themevariant&&(b.vendorthemevariant=a.themevariant.replace("-youtube-light",""),b.themevariant=f.themevariant)}c.previousVersion!==chrome.runtime.getManifest().version&&(b.previousversion=c.previousVersion,b.update=Date.now());0<Object.keys(b).length&&chrome.storage.local.set(b)}});chrome.runtime.onMessage.addListener(async(c,a)=>{switch(c.request){case "pop-up-player":chrome.windows.create(c.options,b=>{chrome.windows.update(b.id,
-{drawAttention:!0})});break;case "options-page":chrome.runtime.openOptionsPage();break;case "whats-new":await chrome.storage.local.set({whatsnew:!1});chrome.tabs.create({url:"html/whats-new.html",active:!0});break;case "dark-theme-off":chrome.storage.local.set({darktheme:!1});break;case "keyboard-shortcuts":chrome.tabs.create({url:"https://www.mrfdev.com/youtube-keyboard-shortcuts",active:!0});break;case "configure-keyboard-shortcuts":chrome.tabs.create({url:"chrome://extensions/shortcuts",active:!0})}});
-chrome.storage.onChanged.addListener(c=>{for(const a in c)void 0!==c[a].newValue&&"customscript"!==a&&"popuplayersize"!==a&&chrome.tabs.query({url:"*://www.youtube.com/*"},b=>{b.forEach(e=>{chrome.tabs.sendMessage(e.id,{message:"preference-changed",name:a,value:c[a].newValue,oldvalue:c[a].oldValue})})})});chrome.commands.onCommand.addListener(c=>{var a={"c070-toggle-loop":"loop","c080-stop-video":"stop","c090-reverse-playlist":"reverse-playlist","c100-toggle-volume-booster":"volume-booster","c130-toggle-annotations":"cards-end-screens",
-"c140-toggle-cinema-mode":"cinema-mode","c150-toggle-player-size":"size","c160-center-video-player":"size","c170-pop-up-player":"pop-up-player","c180-decrease-speed":"speed-minus","c190-increase-speed":"speed-plus","c200-default-speed":"speed","c210-normal-speed":"speed","c220-toggle-video-filters":"video-filters","c230-flip-horizontally":"flip-horizontally","c240-flip-vertically":"flip-vertically","c250-take-screenshot":"screenshot","c260-keyboard-shortcuts":"keyboard-shortcuts","c270-custom-script":"custom-script"};
-switch(c){case "c000-options-page":chrome.runtime.openOptionsPage();break;case "c020-theme-youtube-dark":chrome.storage.local.set({darktheme:!0,theme:"default-dark"});break;case "c025-theme-enhanced-dark":chrome.storage.local.set({darktheme:!0,theme:"enhanced-dark"});break;case "c030-theme-youtube-deep-dark":chrome.storage.local.set({darktheme:!0,theme:"youtube-deep-dark"});break;case "c040-theme-youtube-deep-dark-custom":chrome.storage.local.set({darktheme:!0,theme:"youtube-deep-dark-custom"});break;
-case "c050-theme-custom-theme":chrome.storage.local.get({customtheme:f.customtheme},b=>{chrome.storage.local.set({customtheme:!b.customtheme})});break;default:chrome.tabs.query({lastFocusedWindow:!0,active:!0},b=>{chrome.tabs.sendMessage(b[0].id,{message:"command",command:c,control:a[c]?a[c]:""})})}})})(config);
+
+const handleInstall = async (details) => {
+    if (details.reason === "install") {
+        chrome.runtime.openOptionsPage();
+        chrome.tabs.create({
+            url: `https://www.mrfdev.com/event?n=install&b=chrome&e=enhancer-for-youtube&v=${chrome.runtime.getManifest().version}`,
+            active: true
+        });
+        chrome.storage.local.set({
+            date: Date.now(),
+            update: Date.now()
+        });
+    } else if (details.reason === "update") {
+        const localStorageData = await chrome.storage.local.get();
+        const cacheStorage = {};
+        if (/2\.0\.(10[3-9]|11[0-9]|12[0-1])/.test(details.previousVersion)) {
+            if (Array.isArray(localStorageData.controls)) {
+                cacheStorage.controls = localStorageData.controls.filter(d => d !== "whitelist" && d !== "not-interested");
+            }
+            await chrome.storage.local.remove(["blockads", "blockadsexceptforsubs", "whitelist"]);
+        }
+        if (/^2\.0/.test(details.previousVersion)) {
+            cacheStorage.reload = true;
+            cacheStorage.whatsnew = true;
+            cacheStorage.videofilters = config.videofilters;
+            const filters = "blur brightness contrast grayscale huerotate invert saturate sepia".split(" ");
+            filters.forEach(d => {
+                if (localStorageData[d]) {
+                    if (d === "huerotate") cacheStorage.videofilters.rotation = localStorageData[d];
+                    else if (d === "invert") cacheStorage.videofilters.inversion = localStorageData[d];
+                    else if (d === "saturate") cacheStorage.videofilters.saturation = localStorageData[d];
+                    else cacheStorage.videofilters[d] = localStorageData[d];
+                }
+            });
+            if (localStorageData.backgroundcolor) {
+                cacheStorage.backdropcolor = localStorageData.backgroundcolor;
+                filters.push("backgroundcolor");
+            }
+            if (localStorageData.backgroundopacity) {
+                cacheStorage.backdropopacity = localStorageData.backgroundopacity;
+                filters.push("backgroundopacity");
+            }
+            if (localStorageData.customcssrules) {
+                cacheStorage.customcss = localStorageData.customcssrules;
+                filters.push("customcssrules");
+            }
+            await chrome.storage.local.remove(filters);
+            if (localStorageData.miniplayersize) cacheStorage.miniplayersize = localStorageData.miniplayersize.replace("_", "");
+            if (localStorageData.miniplayerposition) cacheStorage.miniplayerposition = localStorageData.miniplayerposition.replace("_", "");
+            if (localStorageData.themevariant) {
+                cacheStorage.vendorthemevariant = localStorageData.themevariant.replace("-youtube-light", "");
+                cacheStorage.themevariant = config.themevariant;
+            }
+        }
+        if (details.previousVersion !== chrome.runtime.getManifest().version) {
+            cacheStorage.previousversion = details.previousVersion;
+            cacheStorage.update = Date.now();
+        }
+        if (Object.keys(cacheStorage).length > 0) chrome.storage.local.set(cacheStorage);
+    }
+};
+
+const handleMessage = async (message, a) => {
+    switch (message.request) {
+        case "pop-up-player":
+            chrome.windows.create(message.options, b => {
+                chrome.windows.update(b.id, { drawAttention: true });
+            });
+            break;
+        case "options-page":
+            chrome.runtime.openOptionsPage();
+            break;
+        case "whats-new":
+            await chrome.storage.local.set({ whatsnew: false });
+            chrome.tabs.create({ url: "html/whats-new.html", active: true });
+            break;
+        case "dark-theme-off":
+            chrome.storage.local.set({ darktheme: false });
+            break;
+        case "keyboard-shortcuts":
+            chrome.tabs.create({ url: "https://www.mrfdev.com/youtube-keyboard-shortcuts", active: true });
+            break;
+        case "configure-keyboard-shortcuts":
+            chrome.tabs.create({ url: "chrome://extensions/shortcuts", active: true });
+            break;
+    }
+};
+
+const handleStorageChange = (changes) => {
+    for (const a in changes) {
+        if (changes[a].newValue !== undefined && a !== "customscript" && a !== "popuplayersize") {
+            chrome.tabs.query({ url: "*://www.youtube.com/*" }, b => {
+                b.forEach(e => {
+                    chrome.tabs.sendMessage(e.id, {
+                        message: "preference-changed",
+                        name: a,
+                        value: changes[a].newValue,
+                        oldvalue: changes[a].oldValue
+                    });
+                });
+            });
+        }
+    }
+};
+
+const handleCommand = (c) => {
+    const commands = {
+        "c070-toggle-loop": "loop",
+        "c080-stop-video": "stop",
+        "c090-reverse-playlist": "reverse-playlist",
+        "c100-toggle-volume-booster": "volume-booster",
+        "c130-toggle-annotations": "cards-end-screens",
+        "c140-toggle-cinema-mode": "cinema-mode",
+        "c150-toggle-player-size": "size",
+        "c160-center-video-player": "size",
+        "c170-pop-up-player": "pop-up-player",
+        "c180-decrease-speed": "speed-minus",
+        "c190-increase-speed": "speed-plus",
+        "c200-default-speed": "speed",
+        "c210-normal-speed": "speed",
+        "c220-toggle-video-filters": "video-filters",
+        "c230-flip-horizontally": "flip-horizontally",
+        "c240-flip-vertically": "flip-vertically",
+        "c250-take-screenshot": "screenshot",
+        "c260-keyboard-shortcuts": "keyboard-shortcuts",
+        "c270-custom-script": "custom-script"
+    };
+    switch (c) {
+        case "c000-options-page":
+            chrome.runtime.openOptionsPage();
+            break;
+        case "c020-theme-youtube-dark":
+            chrome.storage.local.set({ darktheme: true, theme: "default-dark" });
+            break;
+        case "c025-theme-enhanced-dark":
+            chrome.storage.local.set({ darktheme: true, theme: "enhanced-dark" });
+            break;
+        case "c030-theme-youtube-deep-dark":
+            chrome.storage.local.set({ darktheme: true, theme: "youtube-deep-dark" });
+            break;
+        case "c040-theme-youtube-deep-dark-custom":
+            chrome.storage.local.set({ darktheme: true, theme: "youtube-deep-dark-custom" });
+            break;
+        case "c050-theme-custom-theme":
+            chrome.storage.local.get({ customtheme: config.customtheme }, b => {
+                chrome.storage.local.set({ customtheme: !b.customtheme });
+            });
+            break;
+        default:
+            chrome.tabs.query({ lastFocusedWindow: true, active: true }, b => {
+                chrome.tabs.sendMessage(b[0].id, {
+                    message: "command",
+                    command: c,
+                    control: commands[c] || ""
+                });
+            });
+    }
+};
+
+chrome.action.onClicked.addListener(() => {
+    chrome.runtime.openOptionsPage();
+});
+
+chrome.runtime.setUninstallURL(`https://www.mrfdev.com/event?n=uninstall&b=chrome&e=enhancer-for-youtube&v=${chrome.runtime.getManifest().version}`);
+
+chrome.runtime.onInstalled.addListener(handleInstall);
+
+chrome.runtime.onMessage.addListener(handleMessage);
+
+chrome.storage.onChanged.addListener(handleStorageChange);
+
+chrome.commands.onCommand.addListener(handleCommand);
--- 3_0_5_0/js/youtube-isolated.js
+++ 3_0_5_0/js/youtube-isolated.js
@@ -14,8 +14,117 @@
 ##  © MRFDEV.com - All Rights Reserved
 ##
 */
-(f=>{window.wrappedJSObject||(chrome.runtime.onMessage.addListener(a=>{"command"===a.message?document.dispatchEvent(new CustomEvent("efyt-command",{detail:{command:a.command,control:a.control}})):"preference-changed"===a.message&&document.dispatchEvent(new CustomEvent("efyt-preference-changed",{detail:{name:a.name,value:a.value,oldvalue:a.oldvalue}}))}),document.addEventListener("efyt-message",async a=>{a=a.detail;try{if("pop-up-player"===a.request){var b="https://www.youtube.com/pop-up-player/",
-c=(await chrome.storage.local.get({popuplayersize:f.popuplayersize})).popuplayersize.split("x"),d={request:a.request};a.playlist?(await chrome.storage.local.set({playlist:a.params}),b+=a.params.videos[a.params.index]+"?autoplay=1"):b+=a.params;d.options={url:b,type:"popup",height:parseInt(c[1],10)+9+30,width:parseInt(c[0],10)+16,incognito:chrome.extension.inIncognitoContext,focused:!0};a=d}chrome.runtime.sendMessage(a)}catch(e){document.body.classList.add("efyt-reload-message")}}),document.addEventListener("efyt-get-messages",
-()=>{try{var a=chrome.i18n.getMessage("locale_code"),b="boost_volume brightness cinema_mode color_inversion contrast custom_script expand flip_horizontally flip_vertically gaussian_blur grayscale hue_rotation keyboard_shortcuts locale_dir loop loop_end loop_start message options page_reload_required pop_up_player reset reverse_playlist saturation screenshot sepia shrink speed stop toggle_visibility video_filters".split(" ");chrome.storage.local.get({localecode:f.localecode,whatsnew:f.whatsnew},c=>
-{var d={};if(a===c.localecode){for(var e=b.length-1;0<=e;e--)d[b[e]]=chrome.i18n.getMessage(b[e]);document.dispatchEvent(new CustomEvent("efyt-set-messages",{detail:{messages:d}}))}else fetch(chrome.runtime.getURL(`_locales/${c.localecode}/messages.json`)).then(h=>h.json()).then(h=>{for(var g=b.length-1;0<=g;g--)d[b[g]]=h[b[g]].message;document.dispatchEvent(new CustomEvent("efyt-set-messages",{detail:{messages:d}}))});c.whatsnew&&chrome.runtime.sendMessage({request:"whats-new"})})}catch(c){document.body.classList.add("efyt-reload-message")}}),
-document.addEventListener("efyt-video-filters",a=>{chrome.storage.local.set({filter:a.detail.filter,videofilters:a.detail.videofilters})}),chrome.storage.local.get(f,a=>{document.dispatchEvent(new CustomEvent("efyt-init",{detail:{prefs:a,reload:a.reload,version:chrome.runtime.getManifest().version,previousversion:a.previousversion,themes:chrome.runtime.getURL("css/themes/"),vendorthemes:chrome.runtime.getURL("vendor/themes/")}}));a.reload&&setTimeout(()=>{chrome.storage.local.set({reload:!1})},5E3)}))})(config);
+(f => {
+    const handleMessage = a => {
+        if (a.message === "command") {
+            document.dispatchEvent(new CustomEvent("efyt-command", {
+                detail: {
+                    command: a.command,
+                    control: a.control
+                }
+            }));
+        } else if (a.message === "preference-changed") {
+            document.dispatchEvent(new CustomEvent("efyt-preference-changed", {
+                detail: {
+                    name: a.name,
+                    value: a.value,
+                    oldvalue: a.oldvalue
+                }
+            }));
+        }
+    };
+
+    const handleEfytMessage = async a => {
+        a = a.detail;
+        try {
+            if (a.request === "pop-up-player") {
+                let b = "https://www.youtube.com/pop-up-player/";
+                const playerSize = (await chrome.storage.local.get({ popuplayersize: f.popuplayersize })).popuplayersize.split("x");
+                const d = { request: a.request };
+
+                if (a.playlist) {
+                    await chrome.storage.local.set({ playlist: a.params });
+                    b += `${a.params.videos[a.params.index]}?autoplay=1`;
+                } else {
+                    b += a.params;
+                }
+
+                d.options = {
+                    url: b,
+                    type: "popup",
+                    height: parseInt(playerSize[1], 10) + 9 + 30,
+                    width: parseInt(playerSize[0], 10) + 16,
+                    incognito: chrome.extension.inIncognitoContext,
+                    focused: true
+                };
+                a = d;
+            }
+            chrome.runtime.sendMessage(a);
+        } catch (e) {
+            document.body.classList.add("efyt-reload-message");
+        }
+    };
+
+    const handleEfytGetMessages = () => {
+        try {
+            const localeCode = chrome.i18n.getMessage("locale_code");
+            const videoEffectOptions = "boost_volume brightness cinema_mode color_inversion contrast custom_script expand flip_horizontally flip_vertically gaussian_blur grayscale hue_rotation keyboard_shortcuts locale_dir loop loop_end loop_start message options page_reload_required pop_up_player reset reverse_playlist saturation screenshot sepia shrink speed stop toggle_visibility video_filters".split(" ");
+            chrome.storage.local.get({ localecode: f.localecode, whatsnew: f.whatsnew }, c => {
+                const d = {};
+                if (localeCode === c.localecode) {
+                    videoEffectOptions.forEach(key => d[key] = chrome.i18n.getMessage(key));
+                    document.dispatchEvent(new CustomEvent("efyt-set-messages", {
+                        detail: { messages: d }
+                    }));
+                } else {
+                    fetch(chrome.runtime.getURL(`_locales/${c.localecode}/messages.json`))
+                        .then(h => h.json())
+                        .then(h => {
+                            videoEffectOptions.forEach(key => d[key] = h[key].message);
+                            document.dispatchEvent(new CustomEvent("efyt-set-messages", {
+                                detail: { messages: d }
+                            }));
+                        });
+                }
+                if (c.whatsnew) {
+                    chrome.runtime.sendMessage({ request: "whats-new" });
+                }
+            });
+        } catch (c) {
+            document.body.classList.add("efyt-reload-message");
+        }
+    };
+
+    const handleEfytVideoFilters = a => {
+        chrome.storage.local.set({
+            filter: a.detail.filter,
+            videofilters: a.detail.videofilters
+        });
+    };
+
+    const initialize = a => {
+        document.dispatchEvent(new CustomEvent("efyt-init", {
+            detail: {
+                prefs: a,
+                reload: a.reload,
+                version: chrome.runtime.getManifest().version,
+                previousversion: a.previousversion,
+                themes: chrome.runtime.getURL("css/themes/"),
+                vendorthemes: chrome.runtime.getURL("vendor/themes/")
+            }
+        }));
+        if (a.reload) {
+            setTimeout(() => {
+                chrome.storage.local.set({ reload: false });
+            }, 5000);
+        }
+    };
+
+    window.wrappedJSObject || (
+        chrome.runtime.onMessage.addListener(handleMessage),
+        document.addEventListener("efyt-message", handleEfytMessage),
+        document.addEventListener("efyt-get-messages", handleEfytGetMessages),
+        document.addEventListener("efyt-video-filters", handleEfytVideoFilters),
+        chrome.storage.local.get(f, initialize)
+    );
+})(config);
